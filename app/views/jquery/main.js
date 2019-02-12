efe3b5179cd51d4ed44a554be1ca02d8f396f2c5
Shendidy <sherif@shendidy.com>
2019-02-12 14:23:08 +0000

    fixed mobile interface

---
+++ app/views/jquery/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  $('.menuImg').click(function(){
+    if ( $('.mbLeft').css('display') == 'none' )
+      $('.mbLeft').css('display','block');
+    else
+      $('.mbLeft').css('display','none');
+});
+});
