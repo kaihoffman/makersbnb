6b6f07612bf4f1b769c34c68858ffebe85843c54
Jiseong Park <41450070+JPProgrammingTime@users.noreply.github.com>
2019-02-13 11:31:00 +0000

    Integrated application controller with the routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   resources :users
-  get 'makersbnb/index'
-  get 'index/index'
-  root 'makersbnb#index'
+  root 'application#index'
+  get '/main' => 'application#main'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# Rails.application.routes.draw do
+#   get '/hello_world' => 'application#hello_world'
+# end
