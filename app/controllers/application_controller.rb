8ab2774fdc8fac92b06a65a1cfb105457666cd68
Jiseong Park <41450070+JPProgrammingTime@users.noreply.github.com>
2019-02-14 10:59:25 +0000

    Used active records validate uniqueness method to make sure users are redirected to index when using user names or email that already exists.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,16 @@ class ApplicationController < ActionController::Base
   # end
 
   def create
+
     @new_user = User.new
-    @new_user.add(params['user_name'], params['pswd1'], params['email'], params['full_name'])
-    redirect_to "/main"
+
+    new_user = @new_user.add(params['user_name'], params['pswd1'], params['email'], params['full_name'])
+
+    if new_user.errors.any?
+      redirect_to "/"
+    else
+      redirect_to "/main"
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
 
+  validates_uniqueness_of :user_name, :user_email
+
   def add(user_name, user_password, user_email, full_name)
+
     user = User.create(user_name: user_name, user_password: user_password, user_email: user_email, full_name: full_name)
+
+
   end
 
 end
